d50570cfa19c116f36d89fb9680528eda7b22e8c
tigran <tigr.kman@gmail.com>
2023-05-19 17:03:52 +0300

    React(Code Splitting)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import Counter from "./components/counter";
-import './index.scss';
+import { Suspense } from "react";
+import {Link, Route, Routes} from "react-router-dom";
+import {LazyAboutPage} from "./pages/AboutPage/index.async";
+import {LazyMainPage} from "./pages/MainPage/index.async";
 
 const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<LazyAboutPage />} />
+                    <Route path={'/'} element={<LazyMainPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
