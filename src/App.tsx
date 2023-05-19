38a400cdabe204cfe2ba2907eb90848382ca5649
tigran <tigr.kman@gmail.com>
2023-05-19 18:58:56 +0300

    feat(theme & helper)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Suspense } from "react";
+import {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import {LazyAboutPage} from "./pages/AboutPage/index.async";
 import {LazyMainPage} from "./pages/MainPage/index.async";
+import {useTheme} from "./theme/useTheme";
+import {className} from "./helpers/className";
+import {Theme} from "./theme/ThemeContext";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    const newTheme = theme || Theme.LIGHT;
     return (
-        <div className="app">
+        <div className={className('app', {hovered: true}, [newTheme])}>
+            <button onClick={toggleTheme}>Toggle Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
