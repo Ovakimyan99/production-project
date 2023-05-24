cff34edfbd38e0ab6bc58aed81cdc29397b62277
tigran <tigr.kman@gmail.com>
2023-05-24 15:11:05 +0300

    feat(Sidebar)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import {useTheme} from "./provider/ThemeProvider";
 import {Theme} from "./provider/ThemeProvider/lib/ThemeContext";
 import {AppRouter} from "./provider/router";
 import {Navbar} from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
     const {theme} = useTheme();
@@ -11,6 +12,10 @@ const App = () => {
     return (
         <div className={classNames('app', {hovered: true}, [newTheme])}>
             <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
             <AppRouter />
         </div>
     );
