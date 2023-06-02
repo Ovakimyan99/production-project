8ed1866d0303e9a0a300f6b0e7592150164e719e
tigran <tigr.kman@gmail.com>
2023-06-02 22:12:28 +0300

    feat(ErrorBoundary)

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import { render } from 'react-dom';
 import App from 'app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/provider/ThemeProvider';
+import { ErrorBoundary } from 'app/provider/ErrorBoundary';
 import 'app/styles/index.scss';
 
 import 'shared/config/i18n/i18n';
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root'),
 );
